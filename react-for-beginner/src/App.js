10188e180a76bcecc54f82680b0a2a12a11881bb
Taekyung An <atk0426@naver.com>
2022-07-17 20:07:17 +0900

    useEffect

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,10 +1,16 @@
-import Button from './Button';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue(prev => prev + 1);
+  console.log('i run all the time');
+  useEffect(() => {
+    console.log('call the api..');
+  }, []);
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text={"Countinue"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
