4006e469f53812d1ff584bd09c00e824aa117e6c
Taekyung An <atk0426@naver.com>
2022-07-18 21:36:23 +0900

    To Do List

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,30 +1,35 @@
 import { useState, useEffect } from 'react';
 
-function Hello() {
-  function byFn() {
-    console.log('bye:(');
-  }
-  function hiFn() {
-    console.log('created:)');
-    return byFn;
-  }
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-  // 4번줄 부터 12번줄까지 요약하면 아래와 같음.
-  // useEffect(() => {
-  //   console.log('created :)');
-  //   return console.log('bye :(');
-  // }, []);
-  // return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = event => setToDo(event.target.value);
+  const onSubmit = event => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDo('');
+    setToDos(currentArray => [toDo, ...currentArray]);
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='Write your to do...'
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
