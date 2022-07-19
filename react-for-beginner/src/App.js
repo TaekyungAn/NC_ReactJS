e679f7b057c022cdc3dead1c5ee00c5d25b05600
Taekyung An <atk0426@naver.com>
2022-07-19 21:42:06 +0900

    getting movie from await

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -3,15 +3,17 @@ import { useState, useEffect } from 'react';
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
-    )
-      .then(res => res.json())
-      .then(json => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   console.log(movies);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
