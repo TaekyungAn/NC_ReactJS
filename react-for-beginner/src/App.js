d16149ebf25439aaa58a5aa1544da06b6099e9cc
Taekyung An <atk0426@naver.com>
2022-07-19 21:38:05 +0900

    getting movie from .then

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,59 +1,20 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const onChange = e => {
-    console.log(e.target.value);
-    console.log(e);
-  };
-  const [inverted, setInverted] = useState(false);
-  const [loading, setLoding] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [amount, setAmount] = useState(0);
-  const onInverted = () => {
-    setInverted(current => !current);
-  };
-
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
+    )
       .then(res => res.json())
       .then(json => {
-        setCoins(json);
-        setLoding(false);
+        setMovies(json.data.movies);
+        setLoading(false);
       });
   }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loding...</strong>
-      ) : (
-        <select onChange={onChange}>
-          {coins.map(coin => (
-            <option>
-              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price}USD ={' '}
-              {Math.round((1 / coin.quotes.USD.price) * 10000) / 10000}{' '}
-              {coin.symbol}
-            </option>
-          ))}
-        </select>
-      )}
-      <div>
-        <label htmlFor='dollor'>dollor : </label>
-        <input id='dollor' placeholder='$' type='number' disabled={inverted} />
-      </div>
-      <div>
-        <label htmlFor='coin'>선택된코인이름 : </label>
-        <input
-          id='coin'
-          placeholder='선택된코인이름'
-          type='number'
-          disabled={!inverted}
-        />
-      </div>
-      <button>reset</button>
-      <button onClick={onInverted}>invert</button>
-    </div>
-  );
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
